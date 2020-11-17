83251a755594e48ccf8ce391e91d8308cc45f8f6
brad <jato547@gmail.com>
2020-11-16 20:07:08 -0500

    midi interface debug code

--- src/js/MIDIInterface.js
+++ src/js/MIDIInterface.js
@@ -1,13 +1,26 @@
+// FOUND MIDI DATA
+var MIDI_DATA = {};
+
+// REQUEST MIDI ACCESS, STORE DATA ON PROMISE FULFILL
 navigator.requestMIDIAccess()
   .then(function(access) {
+    // SET MIDI DATA VARS
+    this.access = access;
+    this.inputs = [...this.access.inputs.values()];
+    this.outputs = [...this.access.outputs.values()];
 
-    // Get lists of available MIDI controllers
-    const inputs = access.inputs.values();
-    const outputs = access.outputs.values();
+    // SELECT INPUT AND OUTPUTS
+    this.input = this.inputs[1];
+    this.output = this.outputs[3];
 
-    access.onstatechange = function(e) {
+    // OPEN OUTPUT STREAM
+    this.output.open();
 
-      // Print information about the (dis)connected MIDI controller
-      console.log(e.port.name, e.port.manufacturer, e.port.state);
-    };
-  });
+    // ON INPUT RECV, REPEAT NOTE EVERY SECOND TO OUT
+    this.input.onmidimessage = function(ev) {
+      console.log(ev.data);
+      setInterval(() => this.output.send(ev.data, ev.timestamp), 1000);
+    }.bind(this);
+  }.bind(MIDI_DATA));
+
+export { MIDI_DATA };
